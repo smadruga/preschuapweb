2f3b5dcb26fd59726d295417924f6427ef7d91c6
Rodrigo Campos <campos.rodrigo@ebserh.gov.br>
2022-01-16 22:57:49 -0300

    UPDATE:HOME: Montagem da página de login e configuração da rota apropriada

---
+++ app/Views/home/form_login.php
@@ -0,0 +1,36 @@
+<?= $this->extend('layouts/main'); $this->section('title') ?>
+Entrar <?= $this->endSection() ?>
+<?= $this->section('content') ?>
+
+<main class="form-signin">
+    <?= form_open('login/entrar') ?>
+        <?= csrf_field() ?>
+
+        <div class="form-floating alert alert-danger text-start" role="alert">
+            <?= session()->getFlashdata('error') ?>
+            <?= service('validation')->listErrors() ?>
+        </div>
+
+		<img class="mb-4" src="<?= base_url() ?>/assets/img/caduceus/caduceus-128.png" alt="">
+		<h1 class="h3 mb-3 fw-normal"><?= HUAP_APPNAME ?></h1>
+
+		<div class="form-floating">
+			<input type="text" name="Usuario" class="form-control" id="floatingInput" placeholder="nome.sobrenome@ebserh.gov.br" value="<?= set_value('Usuario') ?>">
+			<label for="floatingInput">Login EBSERH</label>
+		</div>
+		<div class="form-floating">
+			<input type="password" name="Senha" class="form-control" id="floatingPassword" placeholder="Password" value="<?= set_value('Password') ?>">
+			<label for="floatingPassword">Senha</label>
+		</div>
+
+		<div class="checkbox mb-3">
+			<!--<label>
+				<input type="checkbox" value="remember-me"> Remember me
+			</label>-->
+		</div>
+		<button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+		<p class="mt-5 mb-3 text-muted">&copy; 2020 - <?= date('Y') ?></p>
+	</form>
+</main>
+
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= HUAP_APPNAME ?></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="PRESCHUAP WEB - Prescrição médica eletrônica de média e alta complexidade.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#7952b3">
+
+    <link rel="shortcut icon" type="image/png" href="<?= base_url() ?>/favicon.ico"/>
+    <!--<link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">-->
+    <!-- Bootstrap core CSS ThirdParty/img/caduceus-16-->
+    <link href="<?= base_url() ?>/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="assets/css/bootswatch-flatly-bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="<?= base_url() ?>/assets/img/caduceus/caduceus-128.png" sizes="180x180">
+    <link rel="icon" href="<?= base_url() ?>/assets/img/caduceus/caduceus-32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="<?= base_url() ?>/assets/img/caduceus/caduceus-16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="<?= base_url() ?>/favicon.ico">
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="<?= base_url() ?>/assets/bootstrap-5.1.3-examples/sign-in/signin.css" rel="stylesheet">
+
+</head>
+<body class="text-center">
+
+    <?= $this->renderSection('content') ?>
+
+</body>
+</html>
