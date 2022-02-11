2872e11eafc9242f6222a6c24ace3fb8b82ea0b2
Rodrigo Campos <rpcrodrigo@gmail.com>
2022-02-11 04:59:13 -0300

    UPDATE:SELECT2: Adicionei o pacote SELECT2 e seu respectivo tema para o BOOTSTRAP 5. Esse recurso permite montar selects/dropdowns com a possibilidade de pesquisa e uso de ajax

---
+++ public/assets/js/HUAP_ready_jquery.js
@@ -0,0 +1,8 @@
+//Funções e recursos javascript e jQuery inicializadas com o carregamento da página.
+$(document).ready(function() {
+    //inicializa o select2
+    $('select').select2({
+        theme: "bootstrap-5",
+        language: "pt-BR",
+    });
+});
