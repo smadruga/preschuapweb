3debd0d5b146ec06510fe4c5853a1b77cdbb5d7a
Rodrigo Campos <rpcrodrigo@gmail.com>
2022-02-14 17:34:35 -0300

    DEVELOP:COUNTDOWN: Adicionei bibliotecas para contadores em jQuery, ainda avaliando

--- public/assets/js/HUAP_ready_jquery.js
+++ public/assets/js/HUAP_ready_jquery.js
@@ -1,4 +1,4 @@
-//Funções e recursos javascript e jQuery inicializadas com o carregamento da página.
+{//Funções e recursos javascript e jQuery inicializadas com o carregamento da página.
 $(document).ready(function() {
     //inicializa o select2
     $('select').select2({
@@ -10,4 +10,12 @@ $(document).ready(function() {
     $('#submit').one('submit', function() {
         $(this).find('input[type="submit"]').attr('disabled','disabled');
     });
+
+    $('#defaultCountdown').countdown({
+        until: '1h',
+        layout: '{hnn}:{mnn}:{snn}',
+        timezone: -3,
+
+    });
 });
+}
