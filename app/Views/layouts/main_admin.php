b7b103bd9e6ed61f975bee593448861b34ab50ab
Rodrigo Campos <rpcrodrigo@gmail.com>
2022-12-05 13:56:25 -0300

    UPDATE:PRESCHUAP: Bootstrap atualizado para a versão 5.2.3 e fontawesome atualizado para a versão 6.2.1

--- app/Views/layouts/main_admin.php
+++ app/Views/layouts/main_admin.php
@@ -16,7 +16,7 @@
         <link href="<?= base_url('/assets/css/simple-datatables@latest-style.css') ?>" rel="stylesheet">
         <link href="<?= base_url('/assets/css/bootswatch-flatly-bootstrap.min.css') ?>" rel="stylesheet">
         <link href="<?= base_url('/assets/bootstrap-table-1.19.1/dist/bootstrap-table.min.css') ?>" rel="stylesheet">
-        <link href="<?= base_url('/assets/fontawesome-free-6.0.0-web/css/all.min.css') ?>" rel="stylesheet">
+        <link href="<?= base_url('/assets/fontawesome/css/all.min.css') ?>" rel="stylesheet">
 
         <!-- Favicons -->
         <link href="<?= base_url('/favicon.ico') ?>" rel="shortcut icon" type="image/png"/>
