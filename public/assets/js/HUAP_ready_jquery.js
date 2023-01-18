e51ed2fad8bdce2cab781ddfe8a6d26ce04f5828
Rodrigo Campos <rpcrodrigo@gmail.com>
2023-01-18 15:00:17 -0300

    FIX:SELECT2:SEARCH: Corrigido bug ao clicar no select. Agora, ao expandir o dropdown o campo de busca fica habilitado para digitação imediata.

--- public/assets/js/HUAP_ready_jquery.js
+++ public/assets/js/HUAP_ready_jquery.js
@@ -43,3 +43,12 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).on('select2:open', () => {
+    let allFound = document.querySelectorAll('.select2-container--open .select2-search__field');
+    $(this).one('mouseup keyup',()=>{
+        setTimeout(()=>{
+            allFound[allFound.length - 1].focus();
+        },0);
+    });
+});
