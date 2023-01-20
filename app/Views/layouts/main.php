453cc1efef1bd5bcfa2dc8ed691753d9b94bca0e
Rodrigo Campos <rpcrodrigo@gmail.com>
2023-01-20 19:02:09 -0300

    UPDATE:JQUERY: Atualizado o vínculo para arquivo jquery.min.js, de forma a tornar sua atualização mais prática

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -44,7 +44,7 @@
 
     <?= $this->renderSection('content') ?>
 
-    <script src="<?= base_url('/assets/js/jquery-3.6.0.min.js') ?>" crossorigin="anonymous"></script>
+    <script src="<?= base_url('/assets/js/jquery.min.js') ?>" crossorigin="anonymous"></script>
     <script src="<?= base_url('/assets/bootstrap-show-password-1.2.1/dist/bootstrap-show-password.min.js') ?>" crossorigin="anonymous"></script>
 
 </body>
