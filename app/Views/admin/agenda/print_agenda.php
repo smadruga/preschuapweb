a6151fe2a3fb2363c256057232f5812e5efbe386
Rodrigo Campos <rpcrodrigo@gmail.com>
2024-07-25 02:32:47 -0300

    UPDATE:IMPRESSÃO: Ferramenta para impressão da agenda funcional.

--- app/Views/admin/agenda/print_agenda.php
+++ app/Views/admin/agenda/print_agenda.php
@@ -2,177 +2,154 @@
 <?= $this->section('content') ?>
 
 <?php
-foreach($prescricao['array'] as $v) {
+$turnos = ['M' => 'MANHÃ', 'T' => 'TARDE'];
+$totalTurnos = count($turnos);
+$currentTurnoIndex = 0;
 ?>
 
-<table class="table ms-1" style="width:270mm;font-size:70%;">
-    <thead>
-        <tr>
-            <td class="border-0" colspan="12">
+<?php foreach ($turnos as $turno => $turnoNome): ?>
+    <!-- Quebra de página antes de iniciar um novo turno, exceto antes do primeiro turno -->
+    <?php if ($currentTurnoIndex > 0): ?>
+        <div class="page-break"></div>
+    <?php endif; ?>
 
-                <div class="ms-1 me-1">
-                    <div class="row">
-                        <div class="col-5 container border border-dark"><b>
-                            <div class="row">
-                                <div class="col-2 ps-3 pt-2">
-                                    <img src="<?= base_url('/assets/img/huap.png') ?>" width="100%" /><br />
-                                    <img src="<?= base_url('/assets/img/ebserh.png') ?>" width="100%" />
-                                </div>
-                                <div class="col pt-2">
-                                    HOSPITAL UNIVERSITÁRIO ANTONIO PEDRO - HUAP<br />
-                                    GERÊNCIA DE ATENÇÃO A SAÚDE - GAS<br />
-                                    DIVISÃO DE GESTÃO DO CUIDADO - DGC<br />
-                                    UNIDADE DE HEMATOLOGIA E ONCOLOGIA - UHON<br />
-                                    PRESCRIÇÃO MÉDICA DO PACIENTE
-                                </div>
-                            </div>
-                        </b></div>
+    <table class="table table-bordered border-primary" style="width:270mm">
+         <tbody>
 
-                        <div class="col container border border-dark pt-2 border-start-0">
-                            <div class="row">
-                                <div class="col"><b>Prontuário: <?= $v['Prontuario'] ?></b></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>Paciente: <?= $_SESSION['Paciente']['nome'] ?></b></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-5"><b>Nascimento:</b> <?= $_SESSION['Paciente']['dt_nascimento'] ?></div>
-                                <div class="col"><b>Idade:</b> <?= $_SESSION['Paciente']['idade'] ?></div>
-                                <div class="col"><b>Ciclo:</b> <?= $v['Ciclo'] ?></div>
-                                <div class="col"><b>Dia:</b> <?= $v['Dia'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>Peso:</b> <?= $v['Peso'] ?> Kg</div>
-                                <div class="col"><b>Altura:</b> <?= $v['Altura'] ?> cm</div>
-                                <div class="col"><b>IMC:</b> <?= $v['IndiceMassaCorporal'] ?> kg/m²</div>
-                                <div class="col"><b>SC:</b> <?= $v['SuperficieCorporal'] ?> m²</div>
+            <tr>
+                <td colspan="12" >
+                    <div class="ms-1 me-1">
+                        <div class="row">
+                            <div class="col container">
+                                <b>
+                                    <div class="row">
+                                        <div class="col-2 ps-3 pt-2 pb-2">
+                                            <img src="<?= base_url('/assets/img/huap.png') ?>" width="50%" />
+                                        </div>
+                                        <div class="col pt-2 text-center">
+                                            HOSPITAL UNIVERSITÁRIO ANTONIO PEDRO - HUAP<br />
+                                            UNIDADE DE HEMATOLOGIA E ONCOLOGIA - UHON<br />
+                                            <b><h4>AGENDAMENTOS DO DIA: <?= $agenda['data'] ?></h4></b>
+                                        </div>
+                                        <div class="col-2 ps-3 pt-4">
+                                            <img src="<?= base_url('/assets/img/ebserh.png') ?>" width="100%" />
+                                        </div>                                    
+                                    </div>
+                                </b>
                             </div>
                         </div>
                     </div>
+                </td>
+            </tr>
+                
+            <tr class="text-center">
+                <td colspan="12">
+                    <b>########### TURNO: <?= $turnoNome ?> ###########</b>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th>#</th>
+                <th>Tipo</th>
+                <th>Obs</th>            
+                <th>Prontuário</th>
+                <th>Nome</th>
+                <th>Protocolo</th>
+                <th>Medicamento</th>
+                <th>Via</th>
+                <th>Dose</th>
+            </tr>
 
-                </div>
+            <?php
+            if (!isset($agenda['agendamento'][$turno])) {
+                $i = $sq = $inj = $ms = $int = $inc = 0;
+            ?>
+                <tr style="padding: 10px; border: 1px solid; text-align: center;">
+                    <th class="border-0" colspan="12">
+                        ------------------------------------
+                        <i class="fa-solid fa-triangle-exclamation"></i>
+                        SEM AGENDAMENTOS
+                        <i class="fa-solid fa-triangle-exclamation"></i>
+                        ------------------------------------ 
+                    </th>   
+                </tr>
+            <?php
+            } else {
+                $counts = array_fill(1, 5, 0);
+                $i = $sq = $inj = $ms = $int = $inc = 0;
+                foreach ($agenda['agendamento'][$turno] as $v) {
+                    $ant = $agn = '';
+                    foreach ($v as $x) {
+                        if ($i > 0 && $x['idTabPreschuap_TipoAgendamento'] != $agn) {
+                            #echo '<tr><td colspan="9"><br></td></tr>';
+                        }
 
-            </td>
-        </tr>
+                        if ($ant != $x['idPreschuap_Agenda'].'#'.$x['idTabPreschuap_Protocolo']) {
+                            $i++;
+                            $th = ($x['idTabPreschuap_TipoAgendamento'] == 1) ? '<th>'.$i.'</th>' : '<th></th>';
 
-        <tr>
-            <th class="border border-dark">#</th>
-            <th class="border border-dark">Etapa Terapia</th>
-            <th class="border border-dark">Medicamento</th>
-            <th class="border border-dark">Dose do Protocolo</th>
-            <th class="border border-dark">Cálculo Final</th>
-            <th class="border border-dark">Via</th>
-            <th class="border border-dark">Diluente</th>
-            <th class="border border-dark">Vol. (ml)</th>
-            <th class="border border-dark">Tempo Infusão</th>
-            <th class="border border-dark">Posologia</th>
-            <th class="border border-dark col-3">Aprazamento</th>
-        </tr>
+                            $counts[$x['idTabPreschuap_TipoAgendamento']]++;
 
-    </thead>
+                            if($x['idTabPreschuap_TipoAgendamento']==1) $sq++;
+                            if($x['idTabPreschuap_TipoAgendamento']==2) $inj++;
+                            if($x['idTabPreschuap_TipoAgendamento']==3) $ms++;
+                            if($x['idTabPreschuap_TipoAgendamento']==4) $int++;
+                            if($x['idTabPreschuap_TipoAgendamento']==5) $inc++;
 
-    <tbody>
+                            echo "
+                                <tr><td colspan='9'><br></td></tr>
+                                <tr style='padding: 10px; border: 1px solid; text-align: center;'>
+                                    {$th}
+                                    <th>{$x['badge']}</th>
+                                    <th>".esc($x['Observacoes'])."</th>
+                                    <th>".esc($x['Prontuario'])."</th>
+                                    <th>".esc($agenda['paciente'][$x['Prontuario']])."</th>
+                                    <th>".esc($x['Protocolo'])."</th>
+                                    <th>".esc($x['Medicamento'])."</th>
+                                    <th>".esc($x['Codigo'])."</th>
+                                    <th>".esc($x['Dose'])."</th>                                 
+                                </tr>
+                            ";
+                            $ant = $x['idPreschuap_Agenda'].'#'.$x['idTabPreschuap_Protocolo'];
+                        } else {
+                            echo '
+                                <tr style="padding: 10px; border: 1px solid; text-align: center;">
+                                    <th colspan="6"></th>
+                                    <th>'.esc($x['Medicamento']).'</th>
+                                    <th>'.esc($x['Codigo']).'</th>
+                                    <th>'.esc($x['Dose']).'</th>                                 
+                                </tr>
+                            ';                        
+                        }
+                        $agn = $x['idTabPreschuap_TipoAgendamento'];
+                    }
+                }
+            }
+            ?>
+        
+            <tr>
+                <th colspan="9" class="text-center">
+                    <table width="100%">
+                        <tr>
+                            <th><span class="badge bg-primary text-white" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Salão de Quimioterapia"><i class="fa-solid fa-couch"></i></span> <?= $sq ?></th>
+                            <th><span class="badge bg-success text-white" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Injeção"><i class="fa-solid fa-syringe"></i></span> <?= $inj ?></th>
+                            <th><span class="badge bg-warning text-white" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Medicação de Suporte"><i class="fa-solid fa-pills"></i></span> <?= $ms ?></th>
+                            <th><span class="badge bg-danger text-white" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Internação"><i class="fa-solid fa-bed"></i></span> <?= $int ?></th>
+                            <th><span class="badge bg-info text-white" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Intratecal"><i class="fa-solid fa-house-medical"></i></span> <?= $inc ?></th>                        
+                        </tr>
+                    </table>                  
+                </th>
+            </tr>
+            <tr>
+                <th colspan="9" class="text-center">Total: <?= $i ?> agendamentos</th>
+            </tr>
 
-        <?php
-        foreach($medicamento[$v['idPreschuap_Prescricao']] as $m) {
-        ?>
+        </tbody>        
 
-        <tr>
-            <td class="border border-dark"><?= $m['OrdemInfusao'] ?></td>
-            <td class="border border-dark"><?= $m['EtapaTerapia'] ?></td>
-            <td class="border border-dark"><?= $m['Medicamento'] ?></td>
-            <td class="border border-dark"><?= $m['Dose'] ?></td>
-            <td class="border border-dark"><?= $m['Calculo'] ?></td>
-            <td class="border border-dark"><?= $m['ViaAdministracao'] ?></td>
-            <td class="border border-dark"><?= $m['Diluente'] ?></td>
-            <td class="border border-dark"><?= ($m['Volume'] == '0,00' || !$m['Volume'] ) ? NULL : $m['Volume'] . 'ml' ?></td>
-            <td class="border border-dark"><?= $m['TempoInfusao'] ?></td>
-            <td class="border border-dark"><?= $m['Posologia'] ?></td>
-            <td class="border border-dark"></td>
-        </tr>
+    </table>
 
-        <?php
-        }
-        ?>
+    <br><br>
 
-        <tr>
-            <td class="border-0" colspan="12">
-
-                <div class="ms-1 me-1">
-                    <div class="row">
-                        <div class="col container border border-dark border-bottom-0">
-                            <div class="row">
-                                <div class="col"><b>CID-Categoria:</b> <?= $v['Categoria'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>CID-Subcategoria:</b> <?= $v['Subcategoria'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>Aplicabilidade:</b> <?= $v['Aplicabilidade'] ?></div>
-                                <div class="col"><b>Protocolo:</b> <?= $v['Protocolo'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>Data da Prescrição:</b> <?= $v['DataPrescricao'] ?></div>
-                                <div class="col"><b>Tipo de Terapia:</b> <?= $v['TipoTerapia'] ?></div>
-                                <div class="col"><b>Total de Ciclos:</b> <?= $v['CiclosTotais'] ?></div>
-                                <div class="col"><b>Entre Ciclos:</b> <?= $v['EntreCiclos'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><b>Unidade de Internação:</b> --- </div>
-                            </div>
-                            <br />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6 border border-dark">
-                            <div class="col"><b>Informações Complementares:</b>
-                                <?= ($v['InformacaoComplementar']) ? '<br>'.nl2br($v['InformacaoComplementar']) : NULL ?>
-                                <?php
-                                $ajuste = NULL;
-                                foreach($medicamento[$v['idPreschuap_Prescricao']] as $m) {
-                                    $motivo = ($m['MotivoAjusteDose']) ? ' - Motivo: '.$m['MotivoAjusteDose'] : NULL;
-                                    if($m['Ajuste2'] && $m['TipoAjuste'] == 'substituicao')
-                                        $ajuste .= '<br /> Cálculo Final de ' . $m['Medicamento'] . ' substituído por ' . $m['Ajuste2'] . $motivo;
-                                    elseif($m['Ajuste2'] && $m['TipoAjuste'] == 'porcentagem')
-                                        $ajuste .= '<br /> Cálculo Final de ' . $m['Medicamento'] . ' ajustado em ' . $m['Ajuste2'] . $motivo;
-                                }
-                                echo ($ajuste) ? '<br />AJUSTES: ' . $ajuste : NULL;
-                                ?>
-                            </div>
-                        </div>
-                        <div class="col border border-dark border-start-0">
-                            <div class="col"><b>Observações do Protocolo:</b> <?= $v['Observacoes'] ?><br /><br /></div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 border border-dark border-top-0">
-                            <div class="col"><b>Alergias:</b> <?= ($v['Alergia']) ? '<br>'.nl2br($v['Alergia']) : NULL ?></div>
-                        </div>
-                        <div class="col-4 border border-dark border-top-0 border-start-0">
-                            <div class="col"><b>Reações Adversas Anteriores:</b> <?= ($v['ReacaoAdversa']) ? '<br>'.nl2br($v['ReacaoAdversa']) : NULL ?></div>
-                        </div>
-                        <div class="col border border-dark border-top-0 border-start-0">
-                            <div class="col">
-                                <b>Médico(a) Prescritor(a):</b> <?= $v['Nome'] ?><br />
-                                <b>Conselho:</b> <?= $prescricao['conselho'] ?><br /><br />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </td>
-        </tr>
-
-    </tbody>
-
-    <tfoot>
-
-    </tfoot>
-
-</table>
-
-<?php
-}
-?>
+    <?php $currentTurnoIndex++; ?>
+<?php endforeach; ?>
 
 <?= $this->endSection() ?>
