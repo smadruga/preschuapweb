999d5ab44e2236ba2e7a1b27127de8698c22685d
Rodrigo Campos <rpcrodrigo@gmail.com>
2024-07-31 19:13:57 -0300

    DEV:AGENDA: Segue desenvolvimento da tela de estatística de agendamento mental.

--- app/Views/admin/agenda/page_agenda_mes.php
+++ app/Views/admin/agenda/page_agenda_mes.php
@@ -6,6 +6,7 @@
 
         <table class="table">
             <thead>
+
                 <tr>
                     <th>DOM</th>
                     <th>SEG</th>
@@ -18,6 +19,49 @@
             </thead>
 
             <tbody>
+
+                <tr>
+
+                    <?php
+                        
+                        $d = '1';
+                        $m = '2';
+                        $y = '2024';
+                        $x = $y.'-'.$m.'-'.$d;
+                        
+                        $data = new DateTime($x);
+                        $d = $data->format('w');
+                        
+                        $j=0;
+                        #enquanto a data do mês for válida
+                        for($i=0; $i < 31; $i++) {
+
+                            $data = new DateTime($x);
+                            $data->modify('+'.$j.' days');
+                            $d = $data->format('w');
+                            #/*
+                            #enquanto o dia for válido
+                            if ($i >= $d && $m == $data->format('m')) {
+
+                                echo "<td>".$i.' - '.$d.' <#i.ds d#> '.$data->format('d').' m#> '.$data->format('m')."</td>";
+                                echo ($d == '6') ? '</tr><tr>' : NULL;
+                                
+                                $j++;
+
+                            }
+                            else
+                                echo "<td>.$i.</td>";
+                            #*/
+
+                            /*if ($d == 6 && $m < $data->format('m'))
+                                echo '<tr><td>OLAOLAOLA</td></tr>';*/
+
+                        }
+
+
+                    ?>
+                
+                </tr>
 
             </tbody> 
 
