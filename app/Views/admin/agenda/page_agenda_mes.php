6396a565633900c21cfc96a6f800e1bf00287400
Rodrigo Campos <rpcrodrigo@gmail.com>
2024-08-01 01:44:35 -0300

    DEV:AGENDA: Ainda desenvolvendo a tela de agenda mês

--- app/Views/admin/agenda/page_agenda_mes.php
+++ app/Views/admin/agenda/page_agenda_mes.php
@@ -24,37 +24,83 @@
 
                     <?php
                         
-                        $d = '1';
-                        $m = '2';
+                        $d = '01';
+                        $m = '12';
                         $y = '2024';
                         $x = $y.'-'.$m.'-'.$d;
+                                                
+                        $dataorigem = new DateTime($x);
+                        $mo = $dataorigem->format('m');
+                        #$ds = $dataorigem->format('w');
                         
-                        $data = new DateTime($x);
-                        $d = $data->format('w');
-                        
-                        $j=0;
+                        $j=$k=0;
                         #enquanto a data do mês for válida
-                        for($i=0; $i < 31; $i++) {
-
+                        for($i=0; $i < 36; $i++) {
+                            
                             $data = new DateTime($x);
                             $data->modify('+'.$j.' days');
-                            $d = $data->format('w');
+                            $ds = $data->format('w'); #4
+
                             #/*
                             #enquanto o dia for válido
-                            if ($i >= $d && $m == $data->format('m')) {
+                            if ($i >= $ds && $mo == $data->format('m')) {
+                            #if ($i >= $ds) {
 
-                                echo "<td>".$i.' - '.$d.' <#i.ds d#> '.$data->format('d').' m#> '.$data->format('m')."</td>";
-                                echo ($d == '6') ? '</tr><tr>' : NULL;
+
+                                echo "<td><br> i= ".$i.'
+                                <br> ds= '.$ds.'
+                                <br> j= '.$j.'
+                                <br> d= '.$data->format('d').' 
+                                <br> mo= '.$mo.'
+                                <br> ma= '.$data->format('m').'
+                                <br> mf= '.$data->format('m').'
+                                <br> data= '.$data->format('d/m/Y')."
+                                </td>";
+                                echo ($ds == '6') ? '</tr><tr>' : NULL;
                                 
                                 $j++;
 
                             }
-                            else
-                                echo "<td>.$i.</td>";
+                            else {
+                                
+                                $a = ($mo < $data->format('m')) ? 'true' : 'false';
+                                /*
+                                if ($dataorigem->format('Ymd') < $data->format('Ymd')) {
+                                    $j=0;
+                                    $x = $data->format('Y-m-d');
+
+                                } */
+                                #$a = ($m < $data->format('m')) ? $x : 'false';
+                                #$a = ($data->format('Y-m-d') < $data->format('Y-m-d')) ? $x : 'false';
+                                echo "<td>KBO!<br> i= ".$i.'
+                                <br> ds= '.$ds.'
+                                <br> j= '.$j.'
+                                <br> d= '.$data->format('d').' 
+                                <br> m= '.$m.'
+                                <br> mf= '.$data->format('m').'
+                                <br> mo= '.$mo.'
+                                <br> ma= '.$data->format('m').'
+                                <br> a?= '.$a.'
+                                <br> data= '.$data->format('d/m/Y')."
+                                                                
+                                </td>";
+                                
+                                
+
+                                #if ($m < $data->format('m'))
+                                #    echo 'OLAOLAOLA';
+
+                                #echo "<td> KBÔ!!! ".$m.'<#m. m#> '.$data->format('m').' ds.. '.$ds."</td>";
+                                #$j++;
+
+                            }
+                                
                             #*/
 
-                            /*if ($d == 6 && $m < $data->format('m'))
-                                echo '<tr><td>OLAOLAOLA</td></tr>';*/
+                            /*
+                            if ($ds == 6 && $m < $data->format('m'))
+                                echo '<tr><td>OLAOLAOLA</td></tr>';
+                            #*/
 
                         }
 
