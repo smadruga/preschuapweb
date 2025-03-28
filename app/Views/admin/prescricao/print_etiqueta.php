6aa4e4e5287c6e5a331ce94ccfc8fd80992d5608
Rodrigo Campos <rpcrodrigo@gmail.com>
2025-03-28 09:35:37 -0300

    DEV:ETIQUETA: Segue o dev da etiqueta do soro.

--- app/Views/admin/prescricao/print_etiqueta.php
+++ app/Views/admin/prescricao/print_etiqueta.php
@@ -1,86 +1,21 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabela HTML</title>
-    <style>
-        * {
-            margin: 0;
-            padding-left:2px;
-            box-sizing: border-box;
-        }
-        table {
-            width: 94mm; /* Define a largura exata */
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 5px;
-            font-size: 12px;
-        }
-        th {
-            background-color: #f0f0f0;
-        }
-    </style>
-</head>
-<body>
+<?= $this->extend('layouts/main_print') ?>
+<?= $this->section('content') ?>
 
-    <div >
-
-    <table style="margin-left:15px;margin-top:15px;font-size:">
-        <tbody>
-            <tr>
-                <td style="text-align:center;">
-                    <img src="<?= base_url('/logo-hu.png') ?>" width="50%" /><br />
-                    <img src="<?= base_url('/logo-ebserh.png') ?>" width="50%" />
-                </td>
-                <td colspan="2" style="text-align:center;">
-                    MEC/UFF/HUAP/EBSERH<br>
-                    SETOR DE FARMÁCIA HOSPITALAR<br>
-                    CENTRAL DE MANIPULAÇÃO DE QUIMIOTERÁPICOS
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">Paciente: TESTE TESTE TESTE TESTE</td>
-            </tr>
-            <tr>
-                <td width="33%">Pront: 80080080</td>
-                <td width="33%">DtNasc: 21/03/2025</td>
-                <td width="33%">Unid: AMBULATÓRIO QT</td>
-            </tr>
-            <tr>
-                <td colspan="3">Medicamento: DIPIRONA</td>
-            </tr>
-            <tr>
-                <td>Dose(mg): 500</td>
-                <td>Dose vol(ml): 5000</td>
-                <td>Diluente: SIM</td>
-            </tr>
-            <tr>
-                <td>Vol Diluente(ml): 300</td>
-                <td>Vol Final(ml): 300</td>
-                <td>Via Administ: 300</td>
-            </tr>
-            <tr>
-                <td>Fotosensível: SIM</td>
-                <td>Refrigerar: SIM</td>
-                <td>Vesic/Irritante: SIM</td>
-            </tr>
-            <tr>
-                <td>Infusão: 12min</td>
-                <td>Equipo: teste</td>
-                <td>Preparo: 21/03/2025</td>
-            </tr>
-            <tr>
-                <td>Val tmp amb: 22/03/2025</td>
-                <td>Val Refrig: 23/03/2025</td>
-                <td>OBS: ficar atento</td>
-            </tr>
-        </tbody>
-    </table>
-
+<div class="zebra-label">
+    <div class="tabela">
+        <div class="row">
+            <div class="celula">Produto</div>
+            <div class="celula">Lote</div>
+            <div class="celula">Validade</div>
+        </div>
+        <div class="row">
+            <div class="celula">Caneta Azul</div>
+            <div class="celula">A123</div>
+            <div class="celula">12/2025</div>
+        </div>
     </div>
+</div>
 
-</body>
-</html>
+
+
+<?= $this->endSection() ?>
